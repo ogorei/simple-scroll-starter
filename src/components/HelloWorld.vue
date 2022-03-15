fb539560c7e4b20de509bd90735545b16aa02f1e
Reina Ogo <62832934+ogorei@users.noreply.github.com>
2022-03-15 20:20:22 +0900

    starter files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,113 @@
+<template>
+ <section>
+  <h1>スクロールしてください &#8595;</h1>
+</section>
+<section>
+  <div class="container reveal">
+    <h2>セックション１</h2>
+    <div class="text-container">
+      <div class="text-box">
+        <h3>タイトル</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          eius molestiae perferendis eos provident vitae iste.
+        </p>
+      </div>
+      <div class="text-box">
+        <h3>タイトル</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          eius molestiae perferendis eos provident vitae iste.
+        </p>
+      </div>
+      <div class="text-box">
+        <h3>タイトル</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          eius molestiae perferendis eos provident vitae iste.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section>
+  <div class="container ">
+    <h2>セックション２</h2>
+    <div class="text-container">
+      <div class="text-box">
+        <h3>タイトル</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          eius molestiae perferendis eos provident vitae iste.
+        </p>
+      </div>
+      <div class="text-box">
+        <h3>タイトル</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          eius molestiae perferendis eos provident vitae iste.
+        </p>
+      </div>
+      <div class="text-box">
+        <h3>タイトル</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          eius molestiae perferendis eos provident vitae iste.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section>
+  <div class="container ">
+    <h2>セックション３</h2>
+    <div class="text-container">
+      <div class="text-box">
+        <h3>タイトル</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          eius molestiae perferendis eos provident vitae iste.
+        </p>
+      </div>
+      <div class="text-box">
+        <h3>タイトル</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          eius molestiae perferendis eos provident vitae iste.
+        </p>
+      </div>
+      <div class="text-box">
+        <h3>タイトル</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          eius molestiae perferendis eos provident vitae iste.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  name:'HelloWorld',
+  props:{
+    msg: String
+  }
+}
+
+</script>
+
+<style scoped>
+
+section{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color:white
+}
+
+</style>
